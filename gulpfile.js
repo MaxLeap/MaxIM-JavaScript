139a29b4de176b42057a3d4ea44ac2cd2ef35dd1
jjeffcaii <jjeffcaii@outlook.com>
2016-09-14 16:46:29 +0800

    foobar

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+const del = require('del');
 
-gulp.task("default", () => {
+const OUTPUT = 'dist';
+
+gulp.task('clean', cb => {
+    del(OUTPUT, cb)
+});
+
+gulp.task('build', () => {
     return tsProject.src()
         .pipe(ts(tsProject))
-        .js.pipe(gulp.dest("dist"));
+        .js.pipe(gulp.dest(OUTPUT));
 });
+
+gulp.task('default', ['clean', 'build']);
