f980f16c5be0e008c19d51613c26d8abc204cd45
jjeffcaii <jjeffcaii@outlook.com>
2016-09-18 16:52:20 +0800

    foobar

--- test/main.js
+++ test/main.js
@@ -20,37 +20,37 @@ require(['maxim/im'], function (IM) {
     im.login().simple('foo').ok(function (err, session, context) {
         if (err) {
             console.error('login failed: %s', err);
-        } else {
-            document.getElementById('upload').addEventListener('click', function (e) {
-                var file = document.getElementById('attachment').files[0];
-                context.attachment(file).ok(function (err, urls) {
-                    console.log('upload success: %s', urls);
-                });
-            });
-
-            document.getElementById('blobUpload').addEventListener('click', function (e) {
-                var testblob = new Blob(['hello world!'], {type: 'text/plain'});
-                context.attachment(testblob).ok(function (err, urls) {
-                    if(err){
-                        console.error('upload by blob failed: %s',err.message);
-                    }else{
-                        console.log('upload by blob success: %s', urls);
-                    }
-                });
-            });
-
-            context.listFriends(function (err, friends) {
-                friends.forEach(function (friend) {
-                    console.log('>>> [ %s ] - [ %s ]', friend.id, friend.online ? 'ONLLINE' : 'OFFLINE');
-                });
-            });
-
-            setInterval(function () {
-                session.say('hello @' + new Date().getTime()).toFriend('bar').ok();
-            }, 10000);
-
-
-            console.log('welcome %s!', session.current());
+            return;
         }
+        document.getElementById('upload').addEventListener('click', function (e) {
+            var file = document.getElementById('attachment').files[0];
+            context.attachment(file).ok(function (err, urls) {
+                console.log('upload success: %s', urls);
+            });
+        });
+
+        document.getElementById('blobUpload').addEventListener('click', function (e) {
+            var testblob = new Blob(['hello world!'], {type: 'text/plain'});
+            context.attachment(testblob).ok(function (err, urls) {
+                if (err) {
+                    console.error('upload by blob failed: %s', err.message);
+                } else {
+                    console.log('upload by blob success: %s', urls);
+                }
+            });
+        });
+
+        context.listFriends(function (err, friends) {
+            friends.forEach(function (friend) {
+                console.log('>>> [ %s ] - [ %s ]', friend.id, friend.online ? 'ONLLINE' : 'OFFLINE');
+            });
+        });
+
+        setInterval(function () {
+            session.say('hello @' + new Date().getTime()).toFriend('bar').ok();
+        }, 10000);
+
+        console.log('welcome %s!', session.current());
+
     });
 });
